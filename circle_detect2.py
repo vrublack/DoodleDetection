a5f8e5f24a7e964996025ef2dc55ee925fdeccc2
valentin <valentin.rublack@gmail.com>
2017-02-11 21:42:04 -0800

    Optimizing parameters

--- circle_detect2.py
+++ circle_detect2.py
@@ -2,32 +2,77 @@
 import numpy as np
 import argparse
 import cv2
+import os
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image")
-args = vars(ap.parse_args())
 
-# load the image, clone it for output, and then convert it to grayscale
-image = cv2.imread(args["image"])
-output = image.copy()
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def check_model_output(circles, label):
+    if circles is None or len(circles[0]) != 2:
+        return False
+    else:
+        return True
 
-# detect circles in the image
-circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.25, 200, param2=70)
 
-# ensure at least some circles were found
-if circles is not None:
-    # convert the (x, y) coordinates and radius of the circles to integers
-    circles = np.round(circles[0, :]).astype("int")
+def validate_using_params(images, labels, param1, param2, param3):
+    correct = 0
 
-    # loop over the (x, y) coordinates and radius of the circles
-    for (x, y, r) in circles:
-        # draw the circle in the output image, then draw a rectangle
-        # corresponding to the center of the circle
-        cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-        cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+    for i, image in enumerate(images):
+        output = image.copy()
+        gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
 
-    # show the output image
-    cv2.imshow("output", np.hstack([image, output]))
-    cv2.waitKey(0)
+        # detect circles in the image
+        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, param1, param2, param2=param3)
+
+        #print(len(circles[0]))
+
+        if check_model_output(circles, labels[i]):
+            correct += 1
+
+    return correct
+
+
+images = []
+for f in os.listdir('train'):
+    images.append(cv2.imread(os.path.join('train', f)))
+
+labels = [([0, 0, 0, 0], [0, 0, 0, 0]) for _ in images]
+with open(os.path.join('labels', 'front-wheels.txt')) as f:
+    i = 0
+    for l in f.readlines():
+        l = l[:-1]
+        i += 1
+        if len(l) == 0:
+            continue
+        coordinates = l.split(',')
+        for c in range(4):
+            labels[i - 1][0][c] = int(coordinates[c].strip())
+
+with open(os.path.join('labels', 'back-wheels.txt')) as f:
+    i = 0
+    for l in f.readlines():
+        l = l[:-1]
+        i += 1
+        if len(l) == 0:
+            continue
+        coordinates = l.split(',')
+        for c in range(4):
+            labels[i - 1][1][c] = int(coordinates[c].strip())
+
+# labels have only been written up to this point
+images = images[:43]
+labels = labels[:43]
+
+def drange(start, stop, step):
+    r = start
+    while r < stop:
+        yield r
+        r += step
+
+
+for p1 in drange(1.1, 1.6, 0.1):
+    for p2 in range(250, 600, 50):
+        #for p3 in range(40, 150, 10):
+        p3 = 200
+        print('{}, {}, {}:'.format(p1, p2, p3))
+        print(validate_using_params(images, labels, p1, p2, 70))
+
+
