56116669931663d5c6c2838dc20357ebce5e3b77
valentin <valentin.rublack@gmail.com>
2017-02-12 07:28:09 -0800

    Cross validation

--- machine_learning.py
+++ machine_learning.py
@@ -1,10 +1,12 @@
 import pickle
-
 from sklearn import datasets
 from sklearn.svm import SVC
 from scipy import misc
 import os
 import numpy as np
+import random
+
+random.seed(1)
 
 data_dir = 'lights_train'
 
@@ -12,6 +14,8 @@ fnames = []
 for fname in os.listdir(data_dir):
     if fname.startswith('file'):
         fnames.append(fname)
+
+random.shuffle(fnames)
 
 raw_data = [misc.imread(os.path.join(data_dir, fname)) for fname in fnames]
 img_dim = (90, 90)
@@ -23,7 +27,8 @@ for img_i in range(len(raw_data)):
         col = i % raw_data[img_i].shape[1]
         if row < raw_data[img_i].shape[0] and col < raw_data[img_i].shape[1]:
             # average rgb channels to single value
-            avg = (raw_data[img_i][row][col][0] / 3.0 + raw_data[img_i][row][col][1] / 3.0 + raw_data[img_i][row][col][2] / 3.0)
+            avg = (raw_data[img_i][row][col][0] / 3.0 + raw_data[img_i][row][col][1] / 3.0 + raw_data[img_i][row][col][
+                2] / 3.0)
             data[img_i][i] = avg / 255.0
 
 faces = datasets.fetch_olivetti_faces().data
@@ -33,27 +38,36 @@ from sklearn import svm
 X = data
 y = [1 if fname.endswith('True.png') else 0 for fname in fnames]
 
+# retain this fraction for validation
+validation_split = 0.3
+validation_index = int(X.shape[0] * (1 - validation_split))
+
+train_X = X[:validation_index]
+train_y = y[:validation_index]
+
+test_X = X[validation_index:]
+test_y = y[validation_index:]
+
 
 def train():
-    clf = svm.SVC()
-    clf.fit(X, y)
-    SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-        decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
-        max_iter=-1, probability=False, random_state=None, shrinking=True,
-        tol=0.001, verbose=True)
+    clf = SVC(kernel='linear')
+    clf.fit(train_X, train_y)
 
     pickle.dump(clf, open('svc.model', 'w'))
 
     print ("Accuracy on training set:")
-    print (clf.score(X, y))
+    print (clf.score(train_X, train_y))
+
+    print ("Accuracy on validation set:")
+    print (clf.score(test_X, test_y))
 
 
 def predict():
     clf2 = pickle.load(open('svc.model'))
-    Y_predict = clf2.predict(X)
+    Y_predict = clf2.predict(test_X)
     print(Y_predict)
 
 
 train()
 
-predict()
+# predict()
