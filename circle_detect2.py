a9b4a4429c63928de096889572dfe739594aff26
valentin <valentin.rublack@gmail.com>
2017-02-11 22:37:53 -0800

    Getting half of wheel detections right

--- circle_detect2.py
+++ circle_detect2.py
@@ -4,18 +4,49 @@ import argparse
 import cv2
 import os
 
+
+def graph(orig_image, image, circles):
+    # ensure at least some circles were found
+    if circles is not None:
+        # convert the (x, y) coordinates and radius of the circles to integers
+        circles = np.round(circles[:]).astype("int")
+
+        # loop over the (x, y) coordinates and radius of the circles
+        for (x, y, r) in circles:
+            # draw the circle in the output image, then draw a rectangle
+            # corresponding to the center of the circle
+            cv2.circle(image, (x, y), r, (0, 255, 0), 4)
+            cv2.rectangle(image, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+
+        # show the output image
+        cv2.imshow("output", np.hstack([orig_image, image]))
+        cv2.waitKey(0)
+
+
 def check_circle(c, box):
     # is center of circle in box?
     return box[0] <= c[0] <= box[2] and box[1] <= c[1] <= box[3]
 
+
 def check_model_output(circles, label):
-    if circles is None or len(circles[0]) != 2:
+    if circles is None or len(circles) != 2:
         return False
 
-    return check_circle(circles[0][0], label[0]) and check_circle(circles[0][1], label[1]) \
-           or check_circle(circles[0][0], label[1]) and check_circle(circles[0][1], label[0])
+    return check_circle(circles[0], label[0]) and check_circle(circles[1], label[1]) \
+           or check_circle(circles[0], label[1]) and check_circle(circles[1], label[0])
 
-def validate_using_params(images, labels, param1, param2, param3):
+
+def filter(circles):
+    if len(circles) > 2:
+        # filter out circle with higher radius
+        circles = sorted(circles, key=lambda x: x[2])
+        # only return 2 smallest circles
+        return circles[:2]
+    else:
+        return circles
+
+
+def validate_using_params(images, labels, param1, param2, param3, show_graph):
     correct = 0
 
     for i, image in enumerate(images):
@@ -25,7 +56,17 @@ def validate_using_params(images, labels, param1, param2, param3):
         # detect circles in the image
         circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, param1, param2, param2=param3)
 
-        #print(len(circles[0]))
+        if circles is None:
+            continue
+
+        circles = circles[0]
+
+        circles = filter(circles)
+
+        if show_graph and circles is not None:
+            graph(image, output, circles)
+
+        # print(len(circles))
 
         if check_model_output(circles, labels[i]):
             correct += 1
@@ -42,8 +83,10 @@ with open(os.path.join('labels', 'front-wheels.txt')) as f:
     i = 0
     for l in f.readlines():
         l = l[:-1]
-        i += 1
         if len(l) == 0:
+            continue
+        i += 1
+        if l == 'none':
             continue
         coordinates = l.split(',')
         for c in range(4):
@@ -53,29 +96,42 @@ with open(os.path.join('labels', 'back-wheels.txt')) as f:
     i = 0
     for l in f.readlines():
         l = l[:-1]
-        i += 1
         if len(l) == 0:
+            continue
+        i += 1
+        if l == 'none':
             continue
         coordinates = l.split(',')
         for c in range(4):
             labels[i - 1][1][c] = int(coordinates[c].strip())
 
-# labels have only been written up to this point
-images = images[:43]
-labels = labels[:43]
+def optimize():
+    global images
+    global labels
 
-def drange(start, stop, step):
-    r = start
-    while r < stop:
-        yield r
-        r += step
+    def drange(start, stop, step):
+        r = start
+        while r < stop:
+            yield r
+            r += step
+
+    best_result = -1
+    best_params = (-1, -1, -1)
+
+    for p1 in drange(1.1, 1.6, 0.1):
+        for p2 in range(250, 600, 50):
+            # for p3 in range(40, 150, 10):
+            p3 = 70
+            print('{}, {}, {}:'.format(p1, p2, p3))
+            result = validate_using_params(images, labels, p1, p2, p3, False)
+            print(result)
+            if result > best_result:
+                best_result = result
+                best_params = (p1, p2, p3)
+
+    print('Best value {} for params {}, {}, {}'.format(best_result, best_params[0], best_params[1], best_params[2]))
 
 
-for p1 in drange(1.1, 1.6, 0.1):
-    for p2 in range(250, 600, 50):
-        #for p3 in range(40, 150, 10):
-        p3 = 200
-        print('{}, {}, {}:'.format(p1, p2, p3))
-        print(validate_using_params(images, labels, p1, p2, 70))
+#optimize()
 
-
+print(validate_using_params(images, labels, 1.3, 250, 70, False))
